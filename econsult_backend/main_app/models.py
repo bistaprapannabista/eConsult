a67b852ea25e2ca3ddfe7953a5d87eab529fcff6
venutre.prapanna <venture.prapanna@gmail.com>
2024-08-05 20:17:52 +0545

    changes

--- econsult_backend/main_app/models.py
+++ econsult_backend/main_app/models.py
@@ -5,10 +5,58 @@ from django.db import models
 
 class StudentModel(models.Model):
     full_name = models.CharField(max_length=50)
+    # date_of_birth = models.DateField()
     age = models.IntegerField()
+    # gender = models.CharField(max_length=10)
+    # spouse_name = models.CharField(max_length=50)
     nationality = models.CharField(max_length=50)
+    # place_of_birth = models.CharField(max_length=50)
+    # current_address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField(max_length=255)
+
+    # # educational background
+    # elementary_school_name = models.CharField(max_length=50)
+    # elementary_school_date_of_enrollment = models.DateField()
+    # elementary_school_date_of_graduation = models.DateField()
+    # elementary_school_start_date = models.DateField()
+    # elementary_school_end_date = models.DateField()
+
+
+    # junior_high_school_name = models.CharField(max_length=50)
+    # junior_high_school_date_of_enrollment = models.DateField()
+    # junior_high_school_date_of_graduation = models.DateField()
+    # junior_high_school_start_date = models.DateField()
+    # junior_high_school_end_date = models.DateField()
+
+
+    # high_school_name = models.CharField(max_length=50)
+    # high_school_date_of_enrollment = models.DateField()
+    # high_school_date_of_graduation = models.DateField()
+    # high_school_start_date = models.DateField()
+    # high_school_end_date = models.DateField()
+
+    # vocational_school_name = models.CharField(max_length=50)
+    # vocational_school_date_of_enrollment = models.DateField()
+    # vocational_school_date_of_graduation = models.DateField()
+    # vocational_school_start_date = models.DateField()
+    # vocational_school_end_date = models.DateField()
+
+    # university_name = models.CharField(max_length=50)
+    # university_date_of_enrollment = models.DateField()
+    # university_date_of_graduation = models.DateField()
+    # university_start_date = models.DateField()
+    # university_end_date = models.DateField()
+
+    # #work history
+    # name_of_company = models.CharField(max_length=50)
+    # occupation = models.CharField(max_length=50)
+    # date_of_entry = models.DateField()
+    # date_of_retirement = models.DateField()
+
+
+
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- econsult_backend/main_app/views.py
+++ econsult_backend/main_app/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from .models import SchoolFormModel, StudentModel
 from .serializer import SchoolFormSerializer, StudentSerializer
 from rest_framework import generics, status
@@ -7,6 +8,7 @@ from rest_framework.views import APIView
 from django.contrib.auth.models import User
 import os
 from django.conf import settings
+import json
 
 class StudentListCreate(generics.ListCreateAPIView):
     queryset = StudentModel.objects.all()
@@ -24,20 +26,36 @@ class SchoolFormRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = SchoolFormModel.objects.all()
     serializer_class = SchoolFormSerializer
 
+
+def convert_to_numbers_list(arr):
+    if not arr:
+        return []
+    
+    comma_separated_string = arr[0]
+    
+    return [int(x) for x in comma_separated_string.split(',')]
+
 class SchoolFormFillUp(APIView):
 
     def post(self, request, format=None):
 
-        students = StudentModel.objects.all()
-        file_name = SchoolFormModel.objects.get(id=2)
-        file_path = os.path.join(settings.MEDIA_ROOT,str(file_name.file))
-        wb = openpyxl.load_workbook(file_path)
+        school_id = int(request.POST.get('school'))
+        student_ids = convert_to_numbers_list([request.POST.get('students')])
+
+        students = StudentModel.objects.filter(id__in=student_ids)
+        file = SchoolFormModel.objects.get(id=school_id)
+
+        documents_dir = os.path.join(settings.BASE_DIR, 'documents')
+        os.makedirs(documents_dir, exist_ok=True)
+
+        wb = openpyxl.load_workbook(file.file)
+
         sheet = wb["Sheet1"]
         for student in students:
             sheet["B7"] = student.full_name
             sheet["F9"] = student.age
             sheet["B13"] = student.nationality
 
-            save_file_path = f'{student.full_name}.xlsx'
+            save_file_path = os.path.join(documents_dir, f'{student.full_name}-{file.name}-{datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx')
             wb.save(save_file_path)
         return Response({"message":"Form submitted successfully."})
